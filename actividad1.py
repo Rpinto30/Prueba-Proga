f8ffefd49cc52b905af0905b17547a6bc83f807c
Rpinto30 <rodrigopinto3007@gmail.com>
2025-07-15 09:22:58 -0600

    for piramide

--- actividad1.py
+++ actividad1.py
@@ -5,3 +5,7 @@ elif a == 2: print("Adios")
 elif a==3: print("Hola dev")
 else: print("Tas we, chao")
 
+for i in range(int(a)):
+    for j in range(int(a)):
+        if j <=i: print(" * ", end="")
+    print("\n")
